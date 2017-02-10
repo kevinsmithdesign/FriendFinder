8e9c2eebd52a88c724f0d130573d102b02631b7d
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2017-02-10 17:12:25 -0600

    added friends data file

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+//list of pre-set friends
+var friends = [
+	{
+		"name": "Gary",
+		"photo": "http://www.themudflats.net/wp-content/uploads/2014/10/garyjohnsn.jpg",
+		"scores": [5,4,3,1,3,1,3,3,5,5]
+		//33
+	},
+	{
+		"name": "Donald",
+		"photo": "http://az616578.vo.msecnd.net/files/2016/06/13/6360139435793044861461393096_Donald-Trump-prune-face.jpg",
+		"scores": [1,5,4,1,4,5,1,4,5,4]
+		//34
+	},
+	{
+		"name": "Hillary",
+		"photo": "http://static.thefrisky.com/uploads/2014/05/01/hillary-clinton-0050114-600x450.jpg",
+		"scores": [2,2,5,5,3,4,3,1,2,3]
+		//30
+
+	}
+
+];
+//sends to server
+module.exports = friends;
